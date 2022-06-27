28dc19df44021a0707d25d72becc4362b2317605
Zeb Khan <zeb.khan@hotmail.co.uk>
2022-06-27 17:19:10 +0100

    recap.py

---
+++ recap.py
@@ -0,0 +1,10 @@
+# this is how comments are displayed
+print("a simple file")
+greeting = "hello everyone"
+print("this is an example of a string")
+print("1234") # also a string
+print(1234+1) # integer
+print(12.34) # floating point
+print(True) #boolean statement
+print(False) #boolean
+print(None) # null value/ none
