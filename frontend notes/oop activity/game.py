f89001e0fe1af17e758fedcad180e928458db1bb
Zeb Khan <zeb.khan@hotmail.co.uk>
2022-07-04 18:58:59 +0100

    oop activity

---
+++ frontend notes/oop activity/game.py
@@ -0,0 +1,15 @@
+from character import Character
+clarke = Character("Clarke Kent")
+bruce = Character("Bruce Wayne")
+
+clarke.set_hero_name("Superman")
+clarke.set_power("Flies through the air!")
+
+bruce.set_hero_name("Batman")
+bruce.set_power("Drives Batmobile in tight black leather!")
+
+clarke.get_hero_name()
+clarke.get_power()
+
+bruce.get_hero_name()
+bruce.get_power()
