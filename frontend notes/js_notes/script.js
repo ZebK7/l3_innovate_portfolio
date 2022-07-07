de249465f42507d19e1d33a7f2bc60d0c68219b2
Zeb Khan <zeb.khan@hotmail.co.uk>
2022-07-07 18:29:58 +0100

    minor changes

---
+++ frontend notes/js_notes/script.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+let myName = "Zeb";
+console.log(myName);
+
+const fullName ="Mujtabazeb";
+console.log(fullName);
+
+myName ="Mkhan";
+console.log(myName);
+
+if (myName==="Zeb"){
+    console.log("Hello Zeb")
+} else if (myName==="Mkhan"){
+        console.log("Hello Mkhan")
+    } else {
+        console.log("who are you?")
+    }
+
+function sayHello() {
+    console.log("Hello")
+}
+sayHello()
